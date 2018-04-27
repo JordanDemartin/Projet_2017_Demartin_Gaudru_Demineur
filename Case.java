33344c49fc45e4edb8b6189c541b4be88432e0d6
demartin <jordan-demartin@hotmail.fr>
2018-04-27 10:41:40 +0200

    optimisation 1

--- Case.java
+++ Case.java
@@ -120,16 +120,8 @@ public class Case extends JPanel{
 
 	public void saveCase(DataOutputStream flux){
 		try{
-			if(this.bombe){
-				flux.writeInt(1);
-			}else{
-				flux.writeInt(0);
-			}
-			if(this.ouverte){
-				flux.writeInt(1);
-			}else{
-				flux.writeInt(0);
-			}
+			flux.writeBoolean(this.bombe);
+			flux.writeBoolean(this.ouverte);
 			flux.writeInt(this.flag);
 		}catch(IOException e){
 			System.err.println("Erreur de sauvegarde, saveCase");
@@ -138,17 +130,9 @@ public class Case extends JPanel{
 
 	public void setCase(DataInputStream flux){
 		try{
-			if(flux.readInt() == 1){
-				this.bombe=true;
-			}else{
-				this.bombe=false;
-			}
-			if(flux.readInt() == 1){
-				this.ouverte=true;
-			}else{
-				this.ouverte=false;
-			}
-			this.flag=flux.readInt();
+			this.bombe = flux.readBoolean();
+			this.ouverte = flux.readBoolean();
+			this.flag = flux.readInt();
 		}catch(IOException e){
 			System.err.println("Erreur de recuperation des donnees, setCase");
 		}
